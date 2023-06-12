0e89605d1b08b5fa3996165d6af9ad9a73454853
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-06-11 23:37:47 -0400

    done lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
-function App() {
+function App(onAdd,onDelete) {
   const [page, setPage] = useState("List");
-
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((r) => r.json())
+      .then((questions) => {setQuestions(questions)});
+  }, []);
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+  
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAdd={handleAddQuestion} />
+      ) : (
+        <QuestionList
+        
+          onDelete={onDelete}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((r) => r.json())
+      .then((questions) => {setQuestions(questions)});
+  }, []);
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+  function handleDeleteQuestion(deletedQuestion) {
+    const updatedQuestions = questions.filter(
+      (question) => question.id !== deletedQuestion.id
+    );
+    setQuestions(updatedQuestions);
+  }
+   
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+     
+      <ul>{questions.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDelete={handleDeleteQuestion}
+    />
+  ))}</ul>
+
     </section>
   );
 }
